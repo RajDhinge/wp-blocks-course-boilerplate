bef1576171d36a342733cc4e933d0faf898ab8eb
Rajkumar Dhinge <rajkumar.dhinge@wisdmlabs.com>
2022-11-21 20:24:28 +0530

    :zap: Test

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'a2cd94356fe810414779e1d72a0e86e7');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7b5312cd0a366eb734256a74a65f18fc');
--- src/edit.js
+++ src/edit.js
@@ -1,17 +1,90 @@
 import { __ } from '@wordpress/i18n';
-import { useBlockProps, RichText } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	RichText,
+	BlockControls,
+} from '@wordpress/block-editor';
 import './editor.scss';
+import {
+	ToolbarGroup,
+	ToolbarButton,
+	ToolbarDropdownMenu,
+} from '@wordpress/components';
 
 export default function Edit({ attributes, setAttributes }) {
 	const { text } = attributes;
 	return (
-		<RichText
-			{...useBlockProps()}
-			onChange={(value) => setAttributes({ text: value })}
-			value={text}
-			placeholder={__('Your text goes here!', 'text-box')}
-			tagName="h4"
-			allowedFormats={['core/bold']}
-		/>
+		<>
+			<BlockControls
+				controls={[
+					{
+						titl: 'Button 1',
+						icon: 'admin-generic',
+						isActive: true,
+						// eslint-disable-next-line no-console
+						onClick: () => console.log('Button 1'),
+					},
+					{
+						titl: 'Button 2',
+						icon: 'admin-users',
+						isActive: true,
+						// eslint-disable-next-line no-console
+						onClick: () => console.log('Button 2'),
+					},
+				]}
+			>
+				<ToolbarGroup>
+					<ToolbarButton
+						title="Align Left"
+						icon="editor-alignleft"
+						// eslint-disable-next-line no-console
+						onClick={() => console.log('Align Left')}
+					/>
+					<ToolbarButton
+						title="Align Center"
+						icon="editor-aligncenter"
+						// eslint-disable-next-line no-console
+						onClick={() => console.log('Align Center')}
+					/>
+					<ToolbarButton
+						title="Align Right"
+						icon="editor-alignright"
+						// eslint-disable-next-line no-console
+						onClick={() => console.log('Align Right')}
+					/>
+					<ToolbarDropdownMenu
+						icon="editor-aligncenter"
+						controls={[
+							{
+								title: 'Align Left',
+								icon: 'editor-alignleft',
+								// eslint-disable-next-line no-console
+								onClick: () => console.log('Align Left'),
+							},
+							{
+								title: 'Align Center',
+								icon: 'editor-aligncenter',
+								// eslint-disable-next-line no-console
+								onClick: () => console.log('Align Center'),
+							},
+							{
+								title: 'Align Right',
+								icon: 'editor-alignright',
+								// eslint-disable-next-line no-console
+								onClick: () => console.log('Align Right'),
+							},
+						]}
+					/>
+				</ToolbarGroup>
+			</BlockControls>
+			<RichText
+				{...useBlockProps()}
+				onChange={(value) => setAttributes({ text: value })}
+				value={text}
+				placeholder={__('Your text goes here!', 'text-box')}
+				tagName="h4"
+				allowedFormats={['core/bold']}
+			/>
+		</>
 	);
 }
